a26d0ed79bb2e3ceba4a6076984f6fa5ebbf8499
Samantha McGrath <sam.a.mcgrath@gmail.com>
2020-02-25 14:35:16 +1100

    Initial setup

---
+++ src/Calendar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Calendar.css';
+
+class Calendar extends React.Component {
+  render() {
+    return (
+      <div className="calendar">
+        <h2>Calendar</h2>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
