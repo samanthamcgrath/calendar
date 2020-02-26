7e088b6a7f0678d4cf858c279e54b1114e886591
Samantha McGrath <sam.a.mcgrath@gmail.com>
2020-02-26 13:21:18 +1100

    Refactor to use month generator, add month methods

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -2,46 +2,103 @@ import React from 'react';
 import './Calendar.css';
 
 
-interface DayProps {date: Date}
+const monthNames = ["January","February","March","April","May","June","July",
+                    "August","September","October","November","December"];
+const dayNames = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+
+class Year {
+  number: number;
+
+  constructor(number: number) {
+    this.number = number;
+  }
+}
+
+class Month {
+  index: number;
+  year: Year;
+
+  constructor(month: number, year: Year) {
+    this.index = month;
+    this.year = year;
+  }
+
+  getMonthName() {
+    return monthNames[this.index-1];
+  }
+
+  numberOfDaysInMonth() {
+    const daysInMonth = new Date(this.year.number, this.index, 0).getDate();
+    return daysInMonth;
+  }
+
+  *getDaysInMonth() {
+    for (let i = 1; i <= this.numberOfDaysInMonth(); i++) {
+      let day = new FullDate(i, this.index, this.year.number);
+      yield day;
+    }
+  }
+
+}
+
+class FullDate {
+  day: number;
+  month: Month;
+  year: Year;
+
+  constructor(...args: any[]) {
+    if(args.length === 1) {
+      if(args[0] instanceof Date) {
+        this.day = args[0].getDate();
+        this.year = new Year(args[0].getFullYear());
+        this.month = new Month(args[0].getMonth()+1,this.year);
+      } else {
+        throw new Error("argument is not of type Date");
+      }
+    } else {
+      this.day = args[0];
+      this.year = new Year(args[2]);
+      this.month = new Month(args[1],this.year);
+    }
+  }
+}
+
+class MonthPicker extends React.Component {
+  render() {
+    return(
+      <h2>Month name goes here</h2>
+    );
+  }
+}
+
+interface DayProps {date: FullDate}
 
 class Day extends React.Component<DayProps> {
   render() {
     return(
       <button className="day">
-        {this.props.date.getDate()}
+        {this.props.date.day}
       </button>
     );
   }
 }
 
 class MonthCalendar extends React.Component {
-  renderDay(date: Date) {
-    return <Day date={date} key={date.getDate()} />
+  renderDay(date: FullDate): JSX.Element {
+    return <Day date={date} key={date.day} />
   }
 
-  renderMonth(year: number, month: number) {
-    const monthToRender = [];
-
-    //get number of days in current month
-    const daysInMonth = new Date(year, month, 0).getDate();
-    console.log(daysInMonth);
-
-    
-    for (let i = 1; i <= daysInMonth; i++) {
-      let day = new Date(year, month, i);
-      console.log(day);
-      monthToRender.push(this.renderDay(day));
-    }
-    console.log()
-    return monthToRender;
+  renderMonth(month: Month): JSX.Element[] {
+    return Array.from(month.getDaysInMonth()).map(d => this.renderDay(d));
   }
 
   render() {
-    let currentDate = new Date();
+    let currentDate = new FullDate(new Date());
     console.log(currentDate);
+
     return (
       <div className="days">
-        {this.renderMonth(currentDate.getFullYear(),currentDate.getMonth()+1)}
+        {this.renderMonth(currentDate.month)}
       </div>
     );
   }
@@ -52,6 +109,9 @@ class Calendar extends React.Component {
     return (
       <div className="calendar">
         <h2>Calendar</h2>
+        <div className="month-picker">
+          <MonthPicker/>
+        </div>
         <div className="monthly-calendar">
           <MonthCalendar/>
         </div>
